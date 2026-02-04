0b759a65c71b9bfd51830ae7f08613e422e5207d
Phanindra-Nelavalli <nelavalliphanindra4@gmail.com>
2026-02-04 15:06:32 +0530

    Remove base path and add framework detection for Vercel

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/",
   build: {
-    assetsInlineLimit: 0,
     outDir: "dist",
     assetsDir: "assets",
     rollupOptions: {
