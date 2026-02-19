5619b996f31dfb3b43a481f15fb47ffbfe569d6a
MRsanjuedit <tallurisanju1@gmail.com>
2026-02-19 12:49:26 +0530

    Update Season 2 link to new URL

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -104,7 +104,7 @@ function Navbar() {
 
                   <DropdownMenuItem asChild>
                     <a
-                      href="https://www.tedxvishnuinstitute.in/"
+                      href="https://tedxvishuinstitute-season2.vercel.app/"
                       className="block w-full text-white px-3 py-2 text-base lg:text-base xl:text-base font-medium hover:bg-red-600"
                     >
                       Season 2
@@ -199,7 +199,7 @@ function Navbar() {
 
                 <DropdownMenuItem asChild>
                   <a
-                    href="https://www.tedxvishnuinstitute.in/"
+                    href="https://tedxvishuinstitute-season2.vercel.app/"
                     className="block w-full text-white px-3 py-2 text-lg font-medium hover:bg-red-600"
                   >
                     Season 2
